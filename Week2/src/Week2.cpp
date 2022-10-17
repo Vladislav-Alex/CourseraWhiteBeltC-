17e7d38f4c3cbd90a827818da4720597dd4922c9
Vladislav Alex <vladislav.tischenko52@gmail.com>
2022-10-17 12:45:20 +0300

    Updated file Week2. It's only test branch

--- Week2/src/Week2.cpp
+++ Week2/src/Week2.cpp
@@ -13,10 +13,10 @@
 int main() {
 //    cout << Factorial(7);
 //    cout << IsPalindrome("gentleman");
-    vector<string> words = PalindromeFilter({"weew", "code", "X"}, 0);
-    for (auto item : words)
-    {
-        cout << item << " ";
-    }
+    // vector<string> words = PalindromeFilter({"weew", "code", "X"}, 0);
+    // for (auto item : words)
+    // {
+    //     cout << item << " ";
+    // }
     return 0;
 }
